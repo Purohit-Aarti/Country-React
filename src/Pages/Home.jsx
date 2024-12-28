8af6150a0811d8bb486b09b1cfab7a298eb8bce3
aartiPurohit <rti.raj.15nov2002@gmail.com>
2024-12-28 19:27:10 +0530

    created-hero-section-and-navbar

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,24 @@
+import { FaLongArrowAltRight } from "react-icons/fa";
+
 export default function Home () {
     return (
-        <h1>Home Page</h1>
+        <div className="hero-section main">
+            <div className="container grid grid-two-cols">
+                <div className="hero-content">
+                    <h1 className="heading-xl">
+                        Explore the World, One Country at a time.
+                    </h1>
+                    <p className="paragraph">
+                        Discover the history, culture, and beauty of every nation. Sort, search, and filter through the world's countries and learn more about the places you've always wanted to visit.
+                    </p>
+                    <button className="btn btn-darken btn-inline bg-white-box">
+                        Start Exploring <FaLongArrowAltRight />
+                    </button>
+                </div>
+                <div className="hero-image">
+                    <img className="banner-image" src="/Images/world.png" alt="Hero" />
+                </div>
+            </div>
+        </div>
     )
 }
--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,8 +1,34 @@
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
     return (
-        <>
-            <h1>Header</h1>
-        </>
+        <header>
+            <div className="container">
+                <div className="grid navbar-grid">
+                    <div className="Logo">
+                        <NavLink to="/">
+                            <h1>WorldExplorer</h1>
+                        </NavLink>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <NavLink to="/">Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/About">About</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/Contact">Contact</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/Country">Country</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
     );
 };
 
